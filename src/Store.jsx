5306045d7ecee3240d8ca2c2b37366380fc6b390
2Junsu <ijh1205@naver.com>
2022-01-31 22:33:05 +0900

    product reducer 연동

---
+++ src/Store.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {configureStore,createSlice,combineReducers} from '@reduxjs/toolkit'
+import {product} from './reducer'
+
+const slicer = createSlice({
+    name:"Reducer",
+    initialState:{ //스토어에 저장하는 states 초기값
+        isLogin: false,
+        navSelect:"community",
+        selPage:"store",
+    },
+    reducers:{//state를 변경하는 reducers
+        navChange:(state,action)=>{
+            state.navSelect = action.payload;
+        },
+        
+
+    }
+
+})
+
+const rootReducer=combineReducers({slicer:slicer.reducer, product:product.reducer})
+const store= configureStore({reducer: rootReducer}) //redux toolkit 사용가능
+
+export const {navChange} =slicer.actions //export할 reducer들 이름
+export default store;
