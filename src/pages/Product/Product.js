fb80c70a68145942aece185c97603abaa5dcdc00
2Junsu <ijh1205@naver.com>
2022-02-02 01:46:23 +0900

    상품 상세 페이지 Navbar UI 구현

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import Main from '../Main/Main'
-import { ProductSimpleView } from '../Product/components/index'
+import { ProductSimpleView, ProductNavigation } from '../Product/components'
 const Product = () => {
   // 상품 상세정보 불러오는 api 호출할 때 productId 넣어서 보내면 해당 id에 맞는 정보를 서버에서 받아옴
   const productId = useParams().id
@@ -11,6 +11,7 @@ const Product = () => {
     <Container>
       <Main />
       <ProductSimpleView />
+      <ProductNavigation />
     </Container>
   )
 }
