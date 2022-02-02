5e21756da76c5139dd9f73134862463454dd81ac
2Junsu <ijh1205@naver.com>
2022-02-02 20:55:10 +0900

    회원가입 api 연동 test를 위한 코드 작성

---
+++ src/pages/Signup/elements/Checkbox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Checkbox = (props) => {
+  console.log(props)
+  return <Container isClicked={props.isClicked} />
+}
+
+const Container = styled.button`
+  width: 20px;
+  height: 20px;
+  border: 1px solid
+    ${(props) => (props.isClicked ? props.theme.mainColor : '#dddddd')};
+  border-radius: 4px;
+  background-color: ${(props) =>
+    props.isClicked ? props.theme.mainColor : 'white'};
+`
+export default Checkbox
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 const theme = {
   mainColor: '#35c4ef',
   hoverMainColor: '#74a9d4',
+  apiUrl: 'http://localhost8080',
 }
 
 export default theme
