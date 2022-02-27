b38ef93ee934520e77e5535bf73ecad558b78fc0
2Junsu <ijh1205@naver.com>
2022-02-27 22:36:31 +0900

    Design: 화면 비율 조정에 따른 스토리 게시물 상세 페이지 UI 수정

--- src/pages/Story/components/Sidebar.js
+++ src/pages/Story/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React from "react"
-import styled from "styled-components"
-import Element from "../elements/SidebarElement"
+import React from 'react';
+import styled from 'styled-components';
+import Element from '../elements/SidebarElement';
 
 //집들이 게시글 상세 페이지의 좋아요, 스크랩, 댓글, 공유 버튼이 있는 사이드바 컴포넌트
 const Sidebar = () => {
@@ -14,7 +14,7 @@ const Sidebar = () => {
               src="https://img.icons8.com/ios/50/000000/like--v1.png"
               num={1085}
               onClick={() => {
-                alert("dd")
+                alert('dd');
               }}
             />
             <Element
@@ -38,25 +38,25 @@ const Sidebar = () => {
         </SidebarContainer>
       </SidebarWrap>
     </Wrap>
-  )
-}
+  );
+};
 
 const Wrap = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   width: 25%;
   height: 500px;
   position: sticky;
   top: 100px;
-`
+`;
 const SidebarWrap = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 const SidebarContainer = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 const LikeAndScrap = styled.div`
   display: flex;
   flex-direction: column;
@@ -64,13 +64,13 @@ const LikeAndScrap = styled.div`
   justify-content: space-evenly;
   height: 50%;
   border-bottom: 1px solid #dddddd;
-`
+`;
 const CommentAndShare = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
   height: 50%;
-`
+`;
 
-export default Sidebar
+export default Sidebar;
