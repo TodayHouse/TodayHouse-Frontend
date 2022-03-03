d017d4323fffcdfe33c561e4f0d68d20e3271ae5
2Junsu <ijh1205@naver.com>
2022-03-03 14:28:19 +0900

    Add: 상품 등록 페이지 연결

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import {
   CardCollections,
   Setting,
   Seller,
+  UploadProduct,
 } from '../src/pages/index';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
@@ -91,6 +92,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/seller" element={<Seller />}></Route>
+            <Route path="/upload-product" element={<UploadProduct />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
