6f85f660e1cc52d517210fc8397e413d877b4176
2Junsu <ijh1205@naver.com>
2022-05-29 01:34:06 +0900

    Refactor: story 페이지 카테고리 구현 범위 축소에 따른 리팩토링

--- src/pages/Story/components/index.js
+++ src/pages/Story/components/index.js
@@ -1,18 +1,16 @@
-import Category from "./Category"
-import StoryPost from "./StoryPost"
-import Palette from "./Palette"
-import Sidebar from "./Sidebar"
-import FixedMenu from "./FixedMenu"
-import Footer from "./Footer"
-import CategoryElement from "./CategoryElement"
-import DetailBox from "./DetailBox"
+import Category from "./Category";
+import StoryPost from "./StoryPost";
+import Sidebar from "./Sidebar";
+import FixedMenu from "./FixedMenu";
+import Footer from "./Footer";
+import CategoryElement from "./CategoryElement";
+import DetailBox from "./DetailBox";
 export {
-  Category,
-  StoryPost,
-  Palette,
-  Sidebar,
-  FixedMenu,
-  Footer,
-  CategoryElement,
-  DetailBox,
-}
+    Category,
+    StoryPost,
+    Sidebar,
+    FixedMenu,
+    Footer,
+    CategoryElement,
+    DetailBox,
+};
