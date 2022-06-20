e7af02280b9045ea428e6573221d2b33868c921c
2Junsu <ijh1205@naver.com>
2022-06-20 20:57:26 +0900

    Feat: Star component에 별점 선택 기능 추가

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import Rating from 'react-star-ratings';
-import theme from '../theme';
+import React, { useState } from "react";
+import Rating from "react-star-ratings";
+import theme from "../theme";
 
 //rating(0~5 소수점 가능), size(px)를 props로 받아야 함
 //ex) rating={4.5} size="36px"
 const Star = (props) => {
-  return (
-    <Rating
-      rating={props.rating}
-      starRatedColor={theme.mainColor}
-      starEmptyColor="lightgray"
-      numberOfStars={5}
-      name="rating"
-      starDimension={props.size}
-      starSpacing="0px"
-    />
-  );
+    const { size } = props;
+    const [rating, setRating] = useState(0);
+
+    const handleRating = (e) => {
+        setRating(e);
+    };
+    return (
+        <Rating
+            rating={rating}
+            starRatedColor={theme.mainColor}
+            starEmptyColor="lightgray"
+            starHoverColor={theme.mainColor}
+            numberOfStars={5}
+            name="rating"
+            starDimension={props.size}
+            starSpacing="0px"
+            size={size}
+            changeRating={handleRating}
+        />
+    );
 };
 
 export default Star;
