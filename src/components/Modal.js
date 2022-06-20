7a756b48c4ec9ab1cfff375a3b78a7ce8859a9fd
2Junsu <ijh1205@naver.com>
2022-06-20 20:34:26 +0900

    Design: Modal height 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import ModalComponent from "react-modal";
 
 const Modal = (props) => {
-    const { children, width, height, modalOpen, closeModal } = props;
+    const { children, width, modalOpen, closeModal } = props;
 
     return (
         <ModalComponent
@@ -13,17 +13,17 @@ const Modal = (props) => {
                     position: "fixed",
                     top: 0,
                     left: 0,
+                    zIndex: 1,
                 },
                 content: {
                     position: "absolute",
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
-                    // justifyContent: "center",
-                    top: `calc(50% - ${height / 2}px)`,
                     left: `calc(50% - ${width / 2}px)`,
                     width: width + "px",
-                    height: height + "px",
+                    height: "fit-content",
+                    maxHeight: "750px",
                     borderRadius: 8,
                     border: "3px solid skyblue",
                     backgroundColor: "white",
