0c28180bdcfd304aaf01d0b61359c1c62beca3cd
2Junsu <ijh1205@naver.com>
2022-06-25 00:01:23 +0900

    pagination 구현 중

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,41 +1,41 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Page from 'react-js-pagination'
-import '../css/pagination.css'
+import React from "react";
+import styled from "styled-components";
+import Page from "react-js-pagination";
+import "../css/pagination.css";
 
 //totalItems를 props로 받을 예정
-const Pagination = () => {
-  const [page, setPage] = useState(1)
+const Pagination = ({ callApi, page, setPage, totalItemsCount }) => {
+    const handlePageChange = (page) => {
+        setPage(page);
+        callApi(page);
+    };
 
-  const handlePageChange = (page) => {
-    setPage(page)
-  }
-  return (
-    <PaginationContainer>
-      <Page
-        innerClass="paginationBtn"
-        activeClass="active"
-        itemClass="item"
-        itemClassPrev="prev"
-        itemClassNext="next"
-        activePage={page}
-        totalItemsCount={51}
-        itemsCountPerPage={5}
-        pageRangeDisplayed={9}
-        firstPageText={''}
-        lastPageText={''}
-        prevPageText={'<'}
-        nextPageText={'>'}
-        onChange={handlePageChange}
-      />
-    </PaginationContainer>
-  )
-}
+    return (
+        <PaginationContainer>
+            <Page
+                innerClass="paginationBtn"
+                activeClass="active"
+                itemClass="item"
+                itemClassPrev="prev"
+                itemClassNext="next"
+                activePage={page}
+                totalItemsCount={totalItemsCount}
+                itemsCountPerPage={1}
+                pageRangeDisplayed={9}
+                firstPageText={""}
+                lastPageText={""}
+                prevPageText={"<"}
+                nextPageText={">"}
+                onChange={handlePageChange}
+            />
+        </PaginationContainer>
+    );
+};
 
 const PaginationContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-`
-export default Pagination
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+`;
+export default Pagination;
